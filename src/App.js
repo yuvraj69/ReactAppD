aa80529245d23e9e8b6849a54c9774d41a9c53aa
Anjali Garg <garg.anjali132444@gmail.com>
2023-07-10 14:13:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar'
 function App() {
   return (
+    <>
    <h1>Hello</h1>
+   <NavBar/>
+   </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
     <div>
-      
+      <h1>Hello i am nav bar</h1>
     </div>
   )
 }
